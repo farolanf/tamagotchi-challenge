e25be85e9bee343e7f01b275f06a32d6adcb41d4
Farolan Faisal <farolanfaisal@gmail.com>
2023-12-16 01:50:52 +0000

    feat: setup

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div className="bg-blue-200">
+      <header className="flex justify-between">
+        <span>Logo</span>
+        <ul>
+          <li>
+            <button>Login</button>
+          </li>
+        </ul>
+      </header>
+    </div>
+  )
+}
---
+++ src/components/pages/home.tsx
@@ -0,0 +1,10 @@
+import Header from "../header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="bg-red-400">Home</main>
+    </>
+  )
+}
